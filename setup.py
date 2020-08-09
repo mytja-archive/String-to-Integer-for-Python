ba6867f0f8179014c2a8d3efa518f2fd647ddf01
mytja <52399966+mytja@users.noreply.github.com>
2020-08-09 18:49:53 +0200

    setup1

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="String-to-Integer",
+    version="1.0",
+    author="MyTja Team",
+    author_email="mamnju21@gmail.com",
+    description="Convert your string to integer",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mytja/String-to-Integer-for-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.3,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+)
